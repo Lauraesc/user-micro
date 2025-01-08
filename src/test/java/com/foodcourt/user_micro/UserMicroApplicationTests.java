ed95371a3c331a75bc47f95d970d8b627a15b3fb
Laura Escobar Rojo <lauraer442@gmail.com>
2025-01-08 16:52:03 -0500

    my first micro commit

---
+++ src/test/java/com/foodcourt/user_micro/UserMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcourt.user_micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
